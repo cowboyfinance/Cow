a49510a5fddfdc7543aa3fa5ed4212b368517925
cowboyfinance <cowboyfinance>
2020-08-22 20:24:33 +0200

    Gas optimizations

--- Cow.sol
+++ Cow.sol
@@ -92,38 +92,44 @@ contract Cow is Ownable {
         _;
     }
 
+    struct Breeder {
+        uint256 snapshotPeriod;
+        uint256 snapshotBalance;
+    }
+
     // events
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
-    event LogSnapshot(uint period, uint totalAddresses);
-    event LogCandidates(uint period, uint256 totalCandidates, uint number);
-    event LogBreed(uint period, uint256 totalAvailable, uint256 totalAdded, uint totalAdds);
+    event LogWhitelisted(address indexed addr);
+    event LogUnlocked(uint256 timestamp);
     event LogBandits(uint256 totalSupply);
+    event LogBreed(uint256 indexed period, uint256 candidatesLength, uint256 estimatedBreeders, uint256 breededToken, uint256 availableUnits);
 
     // public constants
     string public constant name = "Cow";
     string public constant symbol = "COW";
-    uint8 public constant decimals = 9;
+    uint256 public constant decimals = 9;
+
     // private constants
-    uint256 private constant DECIMALS = 9;
     uint256 private constant MAX_UINT256 = ~uint256(0);
     uint256 private constant INITIAL_TOKENS = 21 * 10**6;
-    uint256 private constant INITIAL_SUPPLY = INITIAL_TOKENS * 10**DECIMALS;
+    uint256 private constant INITIAL_SUPPLY = INITIAL_TOKENS * 10**decimals;
     uint256 private constant TOTAL_UNITS = MAX_UINT256 - (MAX_UINT256 % INITIAL_SUPPLY);
-    uint private constant POOL_SIZE = 50;
-    uint private constant HALVING_PERIOD = 30;
-    uint private constant INIT_POOL_FACTOR = 60;
+    uint256 private constant POOL_SIZE = 50; // 50%
+    uint256 private constant INIT_POOL_FACTOR = 60;
+    uint256 private constant BREED_MIN_BALANCE = 100 * 10**decimals;
+    uint256 private constant BREED_ADDRESS_LIMIT = 1000;
+    uint256 private constant TIMELOCK_TIME = 24 hours;
+    uint256 private constant HALVING_PERIOD = 30;
 
     // mappings
     mapping(address => uint256) private _balances;
-    mapping(address => uint256) private _balancesSnapshot;
-    mapping(address => bool) private _knownAddresses;
     mapping(address => mapping (address => uint256)) private _allowances;
-
-    // arrays
-    address[] private _addresses;
-    address[] private _candidatesList;
-    address[] private _breedersList;
+    mapping(address => bool) private _whitelist;
+    mapping(address => Breeder) private _breeders;
+    mapping(address => bool) private _knownAddresses;
+    mapping(uint256 => address) private _addresses;
+    uint256 _addressesLength;
 
     // ints
     uint256 private _totalSupply;
@@ -131,15 +137,13 @@ contract Cow is Ownable {
     uint256 private _initialPoolToken;
     uint256 private _poolBalance;
     uint256 private _poolFactor;
-    uint256 private _totalBreedersBalance;
 
-    uint private _period;
-    uint private _lockTime;
-    uint private _lockBandits;
+    uint256 private _period;
+    uint256 private _timelockBreeding;
+    uint256 private _timelockBandits;
 
     // bools
-    bool private _lockSnapshot;
-    bool private _lockCandidates;
+    bool private _lockTransfer;
     bool private _lockBreeding;
 
 
@@ -161,151 +165,161 @@ contract Cow is Ownable {
         // set owner balance
         _balances[_owner] = TOTAL_UNITS - _poolBalance;
 
-        // init locks
-        _lockTime = 0;
-        _lockBandits = now.add(24 hours);
-        _lockSnapshot = false;
-        _lockCandidates = true;
+        // init locks & set defaults
+        _lockTransfer = true;
         _lockBreeding = true;
 
-        // we start at period 0, 1 is after first snapshot
-        _period = 0;
+        emit Transfer(address(0x0), _owner, _totalSupply.sub(_initialPoolToken));
+    }
 
-        emit Transfer(address(0x0), _owner, _totalSupply);
+
+    function whitelistAdd(address addr) external onlyOwner {
+        _whitelist[addr] = true;
+        emit LogWhitelisted(addr);
+    }
+
+    // main unlock function
+    // 1. set period
+    // 2. set timelocks
+    // 3. allow token transfer
+    function unlock() external onlyOwner {
+        require(_period == 0, "contract is unlocked");
+        _period = 1;
+        _timelockBreeding = now.add(TIMELOCK_TIME);
+        _timelockBandits = now.add(TIMELOCK_TIME);
+        _lockTransfer = false;
+        _lockBreeding = false;
+        emit LogUnlocked(block.timestamp);
     }
 
 
     // bandits stuff
-    function bandits() external onlyOwner returns (uint256) {
-        require(_lockBandits < now, "also bandits need time to rest");
-        _lockBandits = now.add(24 hours);
+    function bandits() external onlyOwner {
+        require(_lockTransfer == false, "contract is locked");
+        require(_timelockBandits < now, "also bandits need time to rest");
+        _timelockBandits = now.add(TIMELOCK_TIME);
         _totalSupply = _totalSupply.sub(_totalSupply.div(100));
         _unitsPerToken = TOTAL_UNITS.div(_totalSupply);
         emit LogBandits(_totalSupply);
-        return _totalSupply;
     }
 
-
-    // breeding stuff
-    // 1. snapshot(), wait 24h
-    // 2. candidates()
-    // 3. breed()
-
-    // snapshot
-    function snapshot() external onlyOwner returns (bool) {
-        require(_lockSnapshot == false, "snapshot is locked");
-        require(_lockTime < now, "timlock is active");
-        _lockSnapshot = true;
-        _lockTime = now.add(24 hours);
-        _period = _period.add(1);
-        uint addressesLength = _addresses.length;
-        for (uint i=0; i<addressesLength; i++) {
-            address addr = _addresses[i];
-            if(_balancesSnapshot[addr] != _balances[addr]) {
-                _balancesSnapshot[addr] = _balances[addr];
-            }
+    function getSnapshotBalance(address addr) private view returns (uint256) {
+        if (_breeders[addr].snapshotPeriod < _period) {
+            return _balances[addr];
         }
-        if(_period > 1 && (_period % HALVING_PERIOD) == 1) {
-            _poolFactor = _poolFactor.add(_poolFactor);
-        }
-        emit LogSnapshot(_period, addressesLength);
-        _lockCandidates = false;
-        return true;
-    }
-
-    // candidates
-    function candidates() onlyOwner external {
-        require(_lockCandidates == false, "candidates is locked");
-        require(_lockTime < now, "timlock is active");
-        _lockCandidates = true;
-
-        delete _candidatesList;
-        uint addressesLength = _addresses.length;
-        for (uint i=0; i<addressesLength; i++) {
-            address addr = _addresses[i];
-            uint256 snapbalance = _balancesSnapshot[addr];
-            uint256 balance = _balances[addr];
-            if(snapbalance > 0 && balance >= snapbalance) {
-               _candidatesList.push(addr);
-            }
-        }
-
-        delete _breedersList;
-        uint256 totalBreedersBalance = 0;
-
-        uint256 seed = addressesLength+_poolBalance+_period;
-        uint randomNumber = uint(keccak256(abi.encodePacked(seed, now, blockhash(block.number)))) % 10;
-        for (uint i=0; i<_candidatesList.length; i++) {
-            if(i % 10 == randomNumber) {
-                address addr = _candidatesList[i];
-                _breedersList.push(addr);
-                uint256 balance_to_add = _balancesSnapshot[addr].div(_unitsPerToken);
-                totalBreedersBalance = totalBreedersBalance.add(balance_to_add);
-            }
-        }
-
-         _totalBreedersBalance = totalBreedersBalance;
-
-        emit LogCandidates(_period, _breedersList.length, randomNumber);
-        _lockBreeding = false;
+        return  _breeders[addr].snapshotBalance;
     }
 
     // breed
-    function breed() external onlyOwner returns (uint256) {
-        require(_lockBreeding == false, "breed is locked");
-        require(_totalBreedersBalance >= 1, "no pregnant cows, cowboy");
-        _lockBreeding = true;
+    function breed() external onlyOwner {
+        require(_lockTransfer == false, "contract is locked");
+        require(_timelockBreeding < now, "timelock is active");
+        _timelockBreeding = now.add(TIMELOCK_TIME);
 
-        uint breedCount = 0;
-        uint256 available = _initialPoolToken.div(_poolFactor);
-        uint256 available_units = available.mul(_unitsPerToken);
-        uint256 breeded_units = 0;
-        address restBreeder;
+        // need the sum of all breeder balances to calculate share in breed
+        uint256 totalBreedersBalance = 0;
 
-        uint256 totalBreedersBalance = _totalBreedersBalance;
-        _totalBreedersBalance = 0;
-        uint breedersListLength = _breedersList.length;
-        for (uint i=0; i<breedersListLength; i++) {
-            address addr = _breedersList[i];
-            restBreeder = addr;
-            uint256 tokens_to_add = calcShareInTokens(_balancesSnapshot[addr].div(_unitsPerToken), totalBreedersBalance, available_units.div(_unitsPerToken));
-            _balances[addr] = _balances[addr].add(tokens_to_add.mul(_unitsPerToken));
-            breeded_units = breeded_units.add(tokens_to_add.mul(_unitsPerToken));
-            breedCount = breedCount.add(1);
+        // check if address is candidate
+        address[] memory candidates = new address[](_addressesLength);
+        uint256 candidatesLength = 0;
+        for (uint256 i = 0; i < _addressesLength; i++) {
+            address addr = _addresses[i];
+            uint256 snapbalance = getSnapshotBalance(addr);
+            // dont put it on the list if too low
+            if (snapbalance < BREED_MIN_BALANCE.mul(_unitsPerToken)) {
+                continue;
+            }
+            // put it on the list if on of both conditions are true
+            // 1. snapshot is old [no coins moved]
+            // 2. balance >= snapshot balance [no tokens out]
+            if ((_breeders[addr].snapshotPeriod < _period) || (_balances[addr] >= snapbalance)) {
+                candidates[candidatesLength] = addr;
+                candidatesLength++;
+            }
         }
 
-        if((breeded_units < available_units) && (restBreeder != address(0))) {
-            uint256 rest = available_units.sub(breeded_units);
-            _balances[restBreeder] = _balances[restBreeder].add(rest);
-            breeded_units = breeded_units.add(rest);
-        }
-        if(breeded_units > 0) {
-            _poolBalance = _poolBalance.sub(breeded_units);
+        uint256 estimatedBreeders = 0;
+        uint256 breededUnits = 0;
+        uint256 availableUnits = _initialPoolToken.div(_poolFactor).mul(_unitsPerToken);
+        if(candidatesLength > 0) {
+            estimatedBreeders = 1;
+            // get lucky candidates breeders
+            uint256 randomNumber = uint256(keccak256(abi.encodePacked((_addressesLength + _poolBalance + _period), now, blockhash(block.number))));
+            uint256 randomIndex = randomNumber % 10;
+            uint256 randomOffset = 0;
+            if (candidatesLength >= 10) {
+                estimatedBreeders = (candidatesLength - randomIndex - 1) / 10 + 1;
+            }
+            if (estimatedBreeders > BREED_ADDRESS_LIMIT) {
+                randomOffset = (randomNumber / 100) % estimatedBreeders;
+                estimatedBreeders = BREED_ADDRESS_LIMIT;
+            }
+            address[] memory breeders = new address[](estimatedBreeders);
+            uint256 breedersLength = 0;
+            for (uint256 i = 0; i < estimatedBreeders; i++) {
+                address addr = candidates[(randomIndex + (i + randomOffset) * 10) % candidatesLength];
+                breeders[breedersLength] = addr;
+                breedersLength++;
+                totalBreedersBalance = totalBreedersBalance.add(getSnapshotBalance(addr).div(_unitsPerToken));
+            }
+
+
+            for (uint256 i = 0; i < breedersLength; i++) {
+                address addr = breeders[i];
+                uint256 snapbalance = getSnapshotBalance(addr);
+                uint256 tokensToAdd = availableUnits.div(_unitsPerToken).mul(snapbalance.div(_unitsPerToken)).div(totalBreedersBalance);
+                uint256 unitsToAdd = tokensToAdd.mul(_unitsPerToken);
+                _balances[addr] = _balances[addr].add(unitsToAdd);
+                breededUnits = breededUnits.add(unitsToAdd);
+            }
+
+            if ((breededUnits < availableUnits) && (breedersLength > 0)) {
+                address addr = breeders[breedersLength-1];
+                uint256 rest = availableUnits.sub(breededUnits);
+                _balances[addr] = _balances[addr].add(rest);
+                breededUnits = breededUnits.add(rest);
+            }
+            if (breededUnits > 0) {
+                _poolBalance = _poolBalance.sub(breededUnits);
+            }
         }
 
-        emit LogBreed(_period, available, breeded_units.div(_unitsPerToken), breedCount);
-        _lockSnapshot = false;
-        return breeded_units.div(_unitsPerToken);
+        uint256 breededTokens = 0;
+        if(breededUnits > 0) {
+            breededTokens = breededUnits.div(_unitsPerToken);
+        }
+        emit LogBreed(_period, candidatesLength, estimatedBreeders, breededTokens, availableUnits);
+
+        if(_period % HALVING_PERIOD == 0) {
+            _poolFactor = _poolFactor.add(_poolFactor);
+        }
+        _period = _period.add(1);
     }
 
-    function calcShareInTokens(uint256 snapshot_token, uint256 all_breeders_token, uint256 available_token) private pure returns(uint256) {
-        return available_token.mul(snapshot_token).div(all_breeders_token);
+
+    function calcShareInTokens(uint256 snapshotToken, uint256 totalBreedersToken, uint256 availableToken) private pure returns(uint256) {
+        return availableToken.mul(snapshotToken).div(totalBreedersToken);
     }
 
-
-
+    function isOwnerOrWhitelisted(address addr) private view returns (bool) {
+        if (addr == _owner) {
+            return true;
+        }
+        return _whitelist[addr];
+    }
 
     function acquaintAddress(address candidate) private returns (bool) {
         if((_knownAddresses[candidate] != true) && (candidate != _owner)) {
             _knownAddresses[candidate] = true;
-            _addresses.push(candidate);
+            _addresses[_addressesLength] = candidate;
+            _addressesLength++;
             return true;
         }
         return false;
     }
 
 
-    function period() public view returns (uint) {
+    function period() public view returns (uint256) {
         return _period;
     }
 
@@ -321,14 +335,37 @@ contract Cow is Ownable {
         return _balances[account].div(_unitsPerToken);
     }
 
+    function processBreedersBeforeTransfer(address from, address to, uint256 units) private {
+
+        // process sender
+        // if we have no current snapshot, make it
+        // snapshot is balance before sending
+        if(_breeders[from].snapshotPeriod < _period) {
+            _breeders[from].snapshotBalance = _balances[from];
+            _breeders[from].snapshotPeriod = _period;
+        } else {
+            // snapshot is same period, set balance reduced by units (= current balance)
+            _breeders[from].snapshotBalance = _balances[from].sub(units);
+        }
+
+        // process receiver
+        // if we have no current snapshot, make it
+        // snapshot is balance before receiving
+        if(_breeders[to].snapshotPeriod < _period) {
+            _breeders[to].snapshotBalance = _balances[to];
+            _breeders[to].snapshotPeriod = _period;
+        } else {
+            // snapshot is same period, nothing to do -> new tokens have to rest at least 1 period
+            // later in breeding we have also to check the snapshort period and update the balance if < to take care of no transfer/no updated snapshot balance situation
+        }
+    }
 
     function transfer(address recipient, uint256 value) public validRecipient(recipient) returns (bool) {
+        require(((_lockTransfer == false) || isOwnerOrWhitelisted(msg.sender)), 'token transfer is locked');
         uint256 units = value.mul(_unitsPerToken);
         uint256 newSenderBalance = _balances[msg.sender].sub(units);
+        processBreedersBeforeTransfer(msg.sender, recipient, units);
         _balances[msg.sender] = newSenderBalance;
-        if(newSenderBalance < _balancesSnapshot[msg.sender]) {
-            _balancesSnapshot[msg.sender] = newSenderBalance;
-        }
         _balances[recipient] = _balances[recipient].add(units);
         acquaintAddress(recipient);
         emit Transfer(msg.sender, recipient, value);
@@ -336,14 +373,12 @@ contract Cow is Ownable {
     }
 
     function transferFrom(address from, address to, uint256 value) public validRecipient(to) returns (bool) {
-        _balancesSnapshot[from] = 0;
+        require(((_lockTransfer == false) || isOwnerOrWhitelisted(msg.sender)), 'token transfer is locked');
         _allowances[from][msg.sender] = _allowances[from][msg.sender].sub(value);
         uint256 units = value.mul(_unitsPerToken);
+        processBreedersBeforeTransfer(from, to, units);
         uint256 newSenderBalance = _balances[from].sub(units);
         _balances[from] = newSenderBalance;
-        if(newSenderBalance < _balancesSnapshot[from]) {
-            _balancesSnapshot[from] = newSenderBalance;
-        }
         _balances[to] = _balances[to].add(units);
         acquaintAddress(to);
         emit Transfer(from, to, value);
@@ -376,4 +411,5 @@ contract Cow is Ownable {
         emit Approval(msg.sender, spender, _allowances[msg.sender][spender]);
         return true;
     }
+
 }
